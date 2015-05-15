5ba015265561207e5686c55eeb230202faa9899d
duc <nicolas.duc@openwide.fr>
2015-05-15 15:00:11 +0200

    correction nommage conf

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,7 +12,7 @@ class Configuration implements ConfigurationInterface {
      */
     public function getConfigTreeBuilder() {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('openwide_synchronize_ldap_user');
+        $rootNode = $treeBuilder->root('open_wide_synchronize_ldap_user');
 
         $rootNode
                 ->children()
--- DependencyInjection/OpenWideSynchronizeLdapUserExtension.php
+++ DependencyInjection/OpenWideSynchronizeLdapUserExtension.php
@@ -20,19 +20,19 @@ class OpenWideSynchronizeLdapUserExtension extends Extension
 
         if (isset($config['enabled']) && $config['enabled']) {
             $loader->load('services.yml');
-            $container->setParameter('openwide_synchronize_ldap_user.ldap.parent_group.content_id', $config['parent_group_content_id']);
-            $container->setParameter('openwide_synchronize_ldap_user.ldap.parent_group.location_id', $config['parent_group_location_id']);
-            $container->setParameter('openwide_synchronize_ldap_user.ldap.user_password', $config['default_password']);
-            $container->setParameter('openwide_synchronize_ldap_user.post_event_subscriber_ldap.class', $config['event_class']);
-            $container->setParameter('openwide_synchronize_ldap_user.user_helper.class', $config['helper_class']);
-            $container->setParameter('openwide_synchronize_ldap_user.mode', $config['mode']);
-            $container->setParameter('openwide_synchronize_ldap_user.synchronize', $config['synchronize']);
-            $container->setParameter('openwide_synchronize_ldap_user.default_user', $config['default_user']);
-            $container->setParameter('openwide_synchronize_ldap_user.base_dn', $config['ldap']['base_dn']);
-            $container->setParameter('openwide_synchronize_ldap_user.filter_user', $config['ldap']['filter_user']);
-            $container->setParameter('openwide_synchronize_ldap_user.filter_all_user', $config['ldap']['filter_all_user']);
-            $container->setParameter('openwide_synchronize_ldap_user.filter_member_of', $config['ldap']['filter_member_of']);
-            $container->setParameter('openwide_synchronize_ldap_user.field_member_of', $config['ldap']['field_member_of']);
+            $container->setParameter('open_wide_synchronize_ldap_user.ldap.parent_group.content_id', $config['parent_group_content_id']);
+            $container->setParameter('open_wide_synchronize_ldap_user.ldap.parent_group.location_id', $config['parent_group_location_id']);
+            $container->setParameter('open_wide_synchronize_ldap_user.ldap.user_password', $config['default_password']);
+            $container->setParameter('open_wide_synchronize_ldap_user.post_event_subscriber_ldap.class', $config['event_class']);
+            $container->setParameter('open_wide_synchronize_ldap_user.user_helper.class', $config['helper_class']);
+            $container->setParameter('open_wide_synchronize_ldap_user.mode', $config['mode']);
+            $container->setParameter('open_wide_synchronize_ldap_user.synchronize', $config['synchronize']);
+            $container->setParameter('open_wide_synchronize_ldap_user.default_user', $config['default_user']);
+            $container->setParameter('open_wide_synchronize_ldap_user.base_dn', $config['ldap']['base_dn']);
+            $container->setParameter('open_wide_synchronize_ldap_user.filter_user', $config['ldap']['filter_user']);
+            $container->setParameter('open_wide_synchronize_ldap_user.filter_all_user', $config['ldap']['filter_all_user']);
+            $container->setParameter('open_wide_synchronize_ldap_user.filter_member_of', $config['ldap']['filter_member_of']);
+            $container->setParameter('open_wide_synchronize_ldap_user.field_member_of', $config['ldap']['field_member_of']);
             
             $userFieldsLdap = $userFieldsEz = array();
             foreach( $config['fields']['user'] as $key => $field ){
@@ -46,10 +46,10 @@ class OpenWideSynchronizeLdapUserExtension extends Extension
                 $groupFieldsLdap[] = $field['value'];
             }
             
-            $container->setParameter('openwide_synchronize_ldap_user.ldap.fields_user_ldap', $userFieldsLdap);
-            $container->setParameter('openwide_synchronize_ldap_user.ldap.fields_user_ez', $userFieldsEz);
-            $container->setParameter('openwide_synchronize_ldap_user.ldap.fields_group_ldap', $groupFieldsLdap);
-            $container->setParameter('openwide_synchronize_ldap_user.ldap.fields_group_ez', $groupFieldsEz);
+            $container->setParameter('open_wide_synchronize_ldap_user.ldap.fields_user_ldap', $userFieldsLdap);
+            $container->setParameter('open_wide_synchronize_ldap_user.ldap.fields_user_ez', $userFieldsEz);
+            $container->setParameter('open_wide_synchronize_ldap_user.ldap.fields_group_ldap', $groupFieldsLdap);
+            $container->setParameter('open_wide_synchronize_ldap_user.ldap.fields_group_ez', $groupFieldsEz);
         }
     }
 }
